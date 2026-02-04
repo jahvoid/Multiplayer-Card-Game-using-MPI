8f549e3054bbec2776fdd06b8a061295ecb24b95
Miyuki Randle <randlem@sheridancollege.ca>
2026-02-04 10:52:00 -0500

    Updated Placeholders
    Added classes and comments for tasks

--- mpi_card_game.py
+++ mpi_card_game.py
@@ -1,9 +1,20 @@
 from mpi4py import MPI
 
-# Task 1 - Yatharth
+# Task 1: Game Setup - Yatharth
 def createDeck():
     deck = []
 
+# Task 2: Dealer logic - Miyuki
+class Dealer:
+    def __init__(self):
+        self.rank = 0
+
+# Task 3: Player Logic - Jayna
+class Player:
+    def __init__(self, rank):
+        self.rank = rank
+
+# Task 4: Ensure Deadlock Prevention and MPI Termination - Yatharth
 def main():
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
