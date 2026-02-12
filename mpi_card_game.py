c5f0545a80eeb48f758a47643898c7f9839a7863
Miyuki Randle <randlem@sheridancollege.ca>
2026-02-12 02:23:48 -0500

    Updated Dealer/Main Logic

--- mpi_card_game.py
+++ mpi_card_game.py
@@ -2,7 +2,7 @@ from mpi4py import MPI
 import random
 
 # Task 1: Game Setup - Yatharth
-def createDeck():
+def create_deck():
     deck = []
 
     # Temporary
@@ -10,21 +10,38 @@ def createDeck():
 
 # Task 2: Dealer logic - Miyuki
 class Dealer:
-    def __init__(self, player_num, deck):
-        self.rank = 0
-        self.player_num = player_num
+    def __init__(self, num_players, deck):
+        self.num_players = num_players
         self.deck = deck
         self.current_card = None
+        self.player = 1
 
     def deal_hands(self, comm):
-        for i in range(1,self.player_num + 1):
-            hand = random.sample(self.deck, 4)
-            comm.send(hand, dest=i)
+        for p in range(1, self.num_players + 1):
+            hand = [self.deck.pop() for _ in range(4)]
+            comm.send(hand, dest=p)
     
-    def broadcast_card(self, comm):
-        card = random.randint(1, 13)
-        self.current_card = card
-        return comm.bcast(card, root=self.rank)
+    def draw_board_card(self, comm):
+        self.current_card = self.deck.pop()
+    
+    def play_round(self, comm):
+        self.draw_board_card()
+
+        for p in range(1, self.num_players, + 1):
+            # Send turn
+            comm.send(self.current_card, dest=p)
+
+            # Recieve response
+            played_card, status = comm.recv(source=p)
+
+            if status == "pass":
+                self.current_card = played_card
+            elif status == "win":
+                # Notify all players
+                for p in range(1, self.num_players + 1):
+                    comm.send("win", dest=p)
+                return True
+        return False
 
 # Task 3: Player Logic - Jayna
 class Player:
@@ -38,24 +55,35 @@ def main():
     size = comm.Get_size()
     rank = comm.Get_rank()
 
-    deck = createDeck()
+    deck = create_deck()
 
     if rank == 0:
         dealer = Dealer(size -1, deck)
-        card = dealer.current_card
         dealer.deal_hands(comm)
+
+        game_over = False
+        while not game_over:
+            game_over = dealer.play_round(comm)
         
     else:
         player = Player(rank)
-        dealer = None
-        card = None
         player.hand = comm.recv(source=0)
-        print(player.hand)
 
-    dealer = comm.bcast(dealer, root=0)
-    card = dealer.broadcast_card(comm)
+        while True:
+            board_card = comm.recv(source=0)
+            played_card, status = player.take_turn(board_card)
+            comm.send((played_card, status), dest=0)
+
+            if status == "win":
+                break
+
+            result = comm.recv(source=0)
+            if result == "win":
+                break
+
+    #dealer = comm.bcast(dealer, root=0)
+    #card = dealer.broadcast_card(comm)
     
-    print(f"Process {rank}: received {card}")
-
+    #print(f"Process {rank}: received {card}")
 
 main()
